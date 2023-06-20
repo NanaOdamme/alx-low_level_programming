14cce724650cc97caa4afd77ba64cadae41dba4a
NanaOdamme <eronastyles@gmail.com>
2023-06-20 06:25:26 -0700

    task 8 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @u: The first integer
+ * @w: The second integer
+ *
+ * Return: The sum of the two integers
+ */
+int add(int u, int w)
+{
+	int sum = u + w;
+
+	return (sum);
+}
