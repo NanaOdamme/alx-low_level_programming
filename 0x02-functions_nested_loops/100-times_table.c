99644db5b586e7f4166348f1c727a535c11d3de4
NanaOdamme <eronastyles@gmail.com>
2023-06-20 08:13:50 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,47 @@
 #include "main.h"
 
 /**
-* print_times_table - Prints the n times table, starting with 0.
-* @n: The number of rows and columns in the table.
-*
-* Return: void
-*/
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number of rows and columns in the table.
+ *
+ * Return: void
+ */
 void print_times_table(int n)
 {
-	int i, j;
+	int a, mul, result;
 
-	if (n > 15 || n < 0)
+	if (n >= 0 && n <= 15)
 	{
-	return;
-	}
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (a = 0; a <= n; a++)
 		{
-			int result = i * j;
+			_putchar('0');
 
-			if (result > 999)
+			for (mul = 1; mul <= n; mul++)
 			{
-				_putchar((result / 1000) + '0');
+				_putchar(',');
+				_putchar(',');
+
+				result = a * mul;
+
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10)) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+
+				_putchar('\n');
 			}
-
-		if (result > 99)
-		{
-			_putchar(((result / 100) % 10) + '0');
 		}
-
-		if (result > 9)
-		{
-			_putchar(((result / 10) % 10) + '0');
-		}
-
-			_putchar((result % 10) + '0');
-
-		if (j != n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-
-		_putchar('\n');
 	}
 }
+
