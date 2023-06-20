740977ea49aea80e9d53231c87ab7f5f3f7e2c76
NanaOdamme <eronastyles@gmail.com>
2023-06-20 05:33:41 -0700

    task 1 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ */
+void print_alphabet(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	_putchar(character);
+
+	_putchar('\n');
+}
+
