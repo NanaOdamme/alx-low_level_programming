9da50d2526bf09b1fbd60f2d431ef46a4637a421
NanaOdamme <eronastyles@gmail.com>
2023-06-21 06:09:43 -0700

    task main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,8 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
-void positve_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+void positive_or_negative(int i);
 
-#endif
+#endif /* MAIN_H */
