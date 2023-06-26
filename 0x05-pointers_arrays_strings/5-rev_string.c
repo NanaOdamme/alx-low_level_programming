24b5ddd74a282fcd5ea0d287c5ac99a3baae4243
NanaOdamme <eronastyles@gmail.com>
2023-06-26 02:39:37 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,25 +6,25 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int start = 0;
-    int end;
+	int length = 0;
+	int start = 0;
+	int end;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    end = length - 1;
+	end = length - 1;
 
-    while (start < end)
-    {
-        char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+	while (start < end)
+	{
+		char temp = s[start];
 
-        start++;
-        end--;
-    }
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
 
