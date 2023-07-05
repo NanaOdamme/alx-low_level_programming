6472581ce206f95671c3af71a5eff04b1593e50c
NanaOdamme <eronastyles@gmail.com>
2023-07-05 05:37:59 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,33 @@
-#include <stdio.h>
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int p);
 
 /**
- * _sqrt_recursion2 - Recursive2 function to find the square root of n
- * @n: The number to find the square root of
- * @begin: The starting point of the search range
- * @close: The ending point of the search range
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: square of n, or -1 if n does not have a natural square
- */
-int _sqrt_recursion2(int n, int begin, int close)
-{
-	int m;
-	if (begin > close)
-		return (-1);
-
-	       m = (begin + close) / 2;
-
-	if (m * m == n)
-		return (m);
-
-	if (m * m < n)
-		return (_sqrt_recursion2(n, m + 1, close));
-	else
-		return (_sqrt_recursion2(n, begin, m - 1));
-}
-
-/**
- * _sqrt_recursion - Calculates the square of n
- * @n: The number to calculate the square root of
- *
- * Return: The square of n, or -1 if n is negative or does not have natural square
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt_recursion2(n, 1, n));
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the square root of
+ * @p: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int p)
+{
+	if (p * p > n)
+		return (-1);
+	if (p * p == n)
+		return (p);
+	return (actual_sqrt_recursion(n, p + 1));
 }
