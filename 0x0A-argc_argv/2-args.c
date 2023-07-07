1f097a25a7fc21e95dea7c3a3bfdff44f1eefc3c
NanaOdamme <eronastyles@gmail.com>
2023-07-07 06:09:15 -0700

    task 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments received, each on a separate line
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0
+ * 
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc > 0)
+	{
+		printf("%s\n", *argv);
+		argv++;
+		argc--;
+	}
+
+	return (0);
+}
