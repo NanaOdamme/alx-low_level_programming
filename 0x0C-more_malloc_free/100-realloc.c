7913c28e77cf6223277fa952d79056ad7b6ad635
NanaOdamme <eronastyles@gmail.com>
2023-07-13 06:29:41 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_ptr = dest;
+	char *src_ptr = src;
+
+	while (n > 0)
+	{
+		*dest_ptr = *src_ptr;
+		dest_ptr++;
+		src_ptr++;
+		n--;
+	}
+
+	return (dest);
+}
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (!ptr)
+		return malloc(new_size);
+
+	if(!new_size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+	
+	if (!new_ptr)
+	{
+		return (NULL);
+	}
+
+		_memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+
+	free(ptr);
+
+	return (new_ptr);
+}
