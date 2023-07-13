e138048573a41fb93690b7b8d0fb4cf8fdc55ca2
NanaOdamme <eronastyles@gmail.com>
2023-07-13 06:54:33 -0700

    multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -59,7 +59,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, ind, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -70,41 +70,28 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-
-	ind = 0;
-	while (ind <= len1 + len2)
-	{
-		result[ind] = 0;
-		ind++;
-	}
-
-	len1 = len1 - 1;
-	while (len1 >= 0)
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		len2 = _strlen(s2) - 1;
-		while (len2 >= 0)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-			len2--;
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
-		len1--;
 	}
-
-	ind = 0;
-	while (ind < len - 1)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[ind])
+		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[ind] + '0');
-		ind++;
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
