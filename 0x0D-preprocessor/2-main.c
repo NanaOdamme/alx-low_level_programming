9b746bfcb8163d18fc036fd91056dbb0a0c2bfad
NanaOdamme <eronastyles@gmail.com>
2023-07-17 05:57:07 -0700

    betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the current file
+ * main - prints the name of the file
  *
- * Return: 0
- * */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
