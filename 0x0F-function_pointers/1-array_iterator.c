008acbc51c794da7001ead8e642fc7bd93c137bb
NanaOdamme <eronastyles@gmail.com>
2023-07-19 06:15:42 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array && action)
 	{
 		count = 0;
-		while(count < size)
+		while (count < size)
 		{
 			action(array[count]);
 			count++;
