192cbce9e552ba7cc5b5cf77e2e7e7107b3cd9a5
NanaOdamme <eronastyles@gmail.com>
2023-08-01 03:57:31 -0700

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,38 +6,27 @@
  *
  * Return: The number of nodes in the list.
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow, *fast;
-	size_t count = 0;
+size_t count = 0;
+const listint_t *current = head;
 
-	slow = head;
-	fast = head;
-
-	while (fast != NULL && fast->next != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
-
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
-		{
-			slow = head;
-
-			while (1)
-			{
-				printf("[%p] %d\n", (void *)slow, slow->n);
-				count++;
-				slow = slow->next;
-
-				if (slow == fast)
-					break;
-			}
-			break;
-		}
-	}
-	return (count);
+while (current != NULL)
+{
+printf("[%p] %d\n", (const void *)current,  current->n);
+count++;
+if (count == 10)
+{
+break;
 }
+current = current->next;
+}
+
+if (head == NULL)
+{
+exit(98);
+}
+
+return (count);
+}
+
