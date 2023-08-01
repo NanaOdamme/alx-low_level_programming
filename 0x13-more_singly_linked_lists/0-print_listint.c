edf45c2e3dbf09422b06874209fd0e198ce23bb1
NanaOdamme <eronastyles@gmail.com>
2023-08-01 02:46:44 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 size_t count = 0;
+const listint_t *current = h;
 
-while (h != NULL)
+while (current != NULL)
 {
-printf("%d\n", h->n);
-h = h->next;
+printf("%d\n", current->n);
 count++;
+current = current->next;
 }
 
 return (count);
