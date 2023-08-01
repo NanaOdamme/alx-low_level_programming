7288b8ebf878c6f78c9e67045afeb5fd23117ca4
NanaOdamme <eronastyles@gmail.com>
2023-08-01 02:10:10 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,20 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
 	while (*head != NULL)
 	{
-		listint_t *temp = *head;
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
 
 	*head = NULL;
-
 }
+
