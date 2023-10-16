c088db424b8a6d448b82fc053a6b9d45f8b32a79
NanaOdamme <eronastyles@gmail.com>
2023-10-16 10:16:52 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Please make me win!" > gm
-chmod +x gm
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
