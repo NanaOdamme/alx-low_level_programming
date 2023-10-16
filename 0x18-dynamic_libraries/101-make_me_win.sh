fe3598cff9bed7f4ab403d2acf95ab36cbcda24a
NanaOdamme <eronastyles@gmail.com>
2023-10-16 10:07:36 -0700

    gm

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#/bin/bash
-wget -p https://github.com/NanaOdamme/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+#!/bin/bash
+echo "Please make me win!" > gm
+chmod +x gm
